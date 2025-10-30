dde1d5b3da6f5311493e872f9aac33dc04e8c200
OlgaStude <indimindy@yandex.ru>
2025-10-30 13:15:49 +0400

    homework 2

--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,10 +1,14 @@
+import ThemeSwitcher from '../../features/ThemeSwitcher/ui/ThemeSwitcher';
+import Button from '../../shared/ui/Button/Button';
 import styles from './Header.module.css'
 
-function Header(){
+function Header({ children }){
     return (
         <>
             <header className={styles.header}>
-                My Homework project!
+                {children}
+                <ThemeSwitcher></ThemeSwitcher>
+                <p>My Homework project!</p>
             </header>
         </>
     )
