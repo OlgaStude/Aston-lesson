f1db00e325010db987858ceae194b0a9a620389a
OlgaStude <indimindy@yandex.ru>
2025-11-01 09:29:04 +0400

    task 1 Compound Components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,35 +6,24 @@ import MainLayout from '../shared/layouts/MainLayout'
 import { ThemeProvider } from '../shared/lib/theme/ThemeProvider'
 import Modal from '../shared/ui/Modal/Portal'
 import Button from '../shared/ui/Button/Button'
-import styles from "./styles.module.css"
 import { useModal } from '../shared/lib/modal/useModal'
-import { ModalProvider } from '../shared/lib/modal/ModalProvider'
 
 function App() {
 
-  // const [ isModal, switchModal ] = useState(false)
-
-  // const handleClick = () => {
-  //     switchModal(!isModal)
-  //     document.body.style.overflow = isModal ? 'auto' : 'hidden'
-  // }
-
-  const { isModal, handleClick } = useModal();
+  const { isModal } = useModal();
 
   return (
     <>
       <ThemeProvider>
         {
           isModal ? <Modal>
-            <div className={styles.modalContainer}>
-              <Button handleClick={handleClick} title='close' buttonClass="close-modal"></Button>
-              <h2>Учебный проект</h2>
-              <p>Сувориной Ольги</p>
-            </div>
+            <Modal.Container>
+              <Modal.Header></Modal.Header>
+              <Modal.Body></Modal.Body>
+            </Modal.Container>
           </Modal> : null
         }
         <Header>
-              <Button handleClick={handleClick} title='About' buttonClass="about"></Button>
         </Header>
         <MainLayout></MainLayout>
         <Footer></Footer>
--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,12 +1,16 @@
 import ThemeSwitcher from '../../features/ThemeSwitcher/ui/ThemeSwitcher';
+import { useModal } from '../../shared/lib/modal/useModal';
 import Button from '../../shared/ui/Button/Button';
 import styles from './Header.module.css'
 
 function Header({ children }){
+
+    const {handleClick} = useModal()
+
     return (
         <>
             <header className={styles.header}>
-                {children}
+                <Button handleClick={handleClick} title='About' buttonClass="about"></Button>
                 <ThemeSwitcher></ThemeSwitcher>
                 <p>My Homework project!</p>
             </header>
